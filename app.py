5f376bc9020a821e6c69cdeb9e572c8486c686a1
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-11-09 09:19:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name)
 
@@ -6,9 +7,17 @@ app = Flask(__name)
 def index():
     return open("index.html").read()
 
-@app.route('/get_data', methods=['GET'])
-def get_data():
-    return "This is a response from the server."
+@app.route('/fetch_data', methods=['POST'])
+def fetch_data():
+    try:
+        url = request.json['url']
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return "Failed to fetch data from the URL."
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run()
