b5fef9123caee8988c499507a7f715c3cff7ce40
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-11-21 07:43:56 +0900

    Update and rename get_answer.py to main.py

--- get_answer.py
+++
@@ -1,7 +0,0 @@
-from bardapi import Bard
-
-def get_bard_answer(question):
-    token = 'xxxxxxx'  # あなたのBard APIトークン
-    bard = Bard(token=token)
-    answer = bard.get_answer(question)['content']
-    return answer
