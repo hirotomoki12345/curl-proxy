b71d006d710395730584c51fd34c486aa9247b12
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-11-20 18:07:42 +0900

    Update answer_question.py

--- netlify_functions/answer_question.py
+++ netlify_functions/answer_question.py
@@ -1,12 +1,27 @@
-from bardapi import Bard
+// netlify_functions/answerQuestion.js
 
-def handler(event, context):
-    token = 'cQi2uJoenm91t7F3tZO7qet54AK-zydIxB6FufuGH3UDZbwSPdf3n5ZTvMcUC65zHZZrRA.'
-    bard = Bard(token=token)
-    question = event['queryStringParameters']['question']
-    answer = bard.get_answer(question)['content']
+const axios = require('axios');
+
+exports.handler = async function (event, context) {
+  try {
+    const question = event.queryStringParameters.question;
+    const token = 'cQi2uJoenm91t7F3tZO7qet54AK-zydIxB6FufuGH3UDZbwSPdf3n5ZTvMcUC65zHZZrRA.'; // Bard APIのトークンを設定
+    const response = await axios.get(`https://api.bardapi.com/ask?q=${encodeURIComponent(question)}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const answer = response.data.content;
 
     return {
-        'statusCode': 200,
-        'body': answer
-    }
+      statusCode: 200,
+      body: JSON.stringify({ answer }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
