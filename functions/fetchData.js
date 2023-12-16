3eb8848aa35700a0c67725d8dbaec78ff0b1d3c9
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-12-16 20:47:24 +0900

    Update fetchData.js

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -1,25 +1,26 @@
-// netlify/functions/fetchData.js
+// netlify/functions/proxy.js
 const fetch = require('node-fetch');
 
-exports.handler = async function (event, context) {
+exports.handler = async function (event, context, callback) {
+  const targetUrl = 'http://localhost:9000';  // あなたのターゲットURLに置き換えてください
+  const requestUrl = event.queryStringParameters.url;
+
   try {
-    // フロントエンドから送信されたURLを取得
-    const { url } = JSON.parse(event.body);
+    const response = await fetch(`${targetUrl}?url=${requestUrl}`, {
+      method: 'GET',
+      headers: event.headers,
+    });
 
-    // 外部のURLからデータをフェッチ
-    const response = await fetch(url);
-    const data = await response.json();
+    const data = await response.text();
 
-    // データを返す
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
+    callback(null, {
+      statusCode: response.status,
+      body: data,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   } catch (error) {
-    // エラーが発生した場合はエラーレスポンスを返す
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
-    };
+    callback(error);
   }
 };
